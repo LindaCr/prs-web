ff828a6f30d16302fea4bd0a079ed5c9c93d6988
LindaCr <lindacrook.44@gmail.com>
2021-06-28 20:30:02 -0400

    init commit

---
+++ src/main/java/com/prs/web/LineItemController.java
@@ -0,0 +1,62 @@
+package com.prs.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.prs.business.LineItem;
+import com.prs.db.LineItemRepo;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/lineitems")
+public class LineItemController {
+
+	@Autowired
+	private LineItemRepo lineItemRepo;
+	
+	@GetMapping("/")
+	public Iterable<LineItem> getAll() {
+		return lineItemRepo.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<LineItem> get(@PathVariable Integer id) {
+		return lineItemRepo.findById(id);
+	}
+	
+	@PostMapping("/")
+	public LineItem add(@RequestBody LineItem lineItem) {
+		return lineItemRepo.save(lineItem);
+	}
+	
+	@PutMapping("/")
+	public LineItem update(@RequestBody LineItem lineItem) {
+		return lineItemRepo.save(lineItem);
+	}
+	
+	@DeleteMapping("/{id}")
+	public Optional<LineItem> delete(@PathVariable Integer id) {
+		Optional<LineItem> lineItem=lineItemRepo.findById(id);
+		if (lineItem.isPresent()) {
+			try {
+			lineItemRepo.deleteById(id);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+						"Exception caught during lineItem delete");
+			}
+		}
+		else {
+			System.err.println("LineItem delete error- no lineItem found for id"+id);
+		}
+		return lineItem;
+	}
+	
+	
+	
+}
